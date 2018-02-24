0f9d6df6cd29df9631eea81cd4c57e38a09523be
Simona Kurňavová <kurnavova.simona@gmail.com>
2018-02-24 17:04:39 +0100

    Serializers added

---
+++ dashboard/dash_app/serializers.py
@@ -0,0 +1,26 @@
+from dash_app.models import *
+from rest_framework import serializers
+
+
+class AppSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = App
+        fields = ('id', 'name', 'description', 'required_account')
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ('owner', 'type', 'name', 'token', 'info')
+
+
+class WidgetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Widget
+        fields = ('app', 'account', 'position_x', 'position_y')
+
+
+class DashboardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Dashboard
+        fields = ('owner', 'widgets')
