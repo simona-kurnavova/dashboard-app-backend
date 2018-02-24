3cb3ea3b064a7341bce589403f5ae73c41514489
Simona Kurňavová <kurnavova.simona@gmail.com>
2018-02-24 17:03:20 +0100

    App creation and admin setup

---
+++ dashboard/dash_app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from dash_app.models import *
+# Register your models here.
+
+admin.site.register(App)
+admin.site.register(Account)
+admin.site.register(Widget)
+admin.site.register(Dashboard)
---
+++ dashboard/dash_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DashAppConfig(AppConfig):
+    name = 'dash_app'
